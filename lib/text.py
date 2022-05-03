2efb07d3fc6a926435698f3190e30b057b6d87a1
kmyukie <kmoriyukie@gmail.com>
2022-05-03 12:26:43 +0100

    Adicionei /lib e um arquivo que conterá as classes

---
+++ lib/text.py
@@ -0,0 +1,12 @@
+class word:
+    def __init__(self, text, video):
+        self.text = text
+        self.video = video
+    def on_hover(self):
+        pass
+
+class phrase:
+    def __init__(self):
+        self.words = []
+    def display(self):
+        pass
