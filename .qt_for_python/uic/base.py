662e530899221c1e1332e60dfd444b570d5827fe
Lucyanno Frota <lucyannofrota@gmail.com>
2022-05-15 15:04:33 +0100

    Ajuste das imagens

--- .qt_for_python/uic/base.py
+++ .qt_for_python/uic/base.py
@@ -40,3 +40,4 @@ class Ui_MainWindow(object):
         _translate = QtCore.QCoreApplication.translate
         MainWindow.setWindowTitle(_translate("MainWindow", "MainWindow"))
 
+import menus_resources_rc
