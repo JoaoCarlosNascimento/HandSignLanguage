355e595cc72456ee968c551d92caaec2a9a0c5eb
Robaleco <62552442+Robaleco@users.noreply.github.com>
2022-05-16 14:25:19 +0100

    Update

--- ReadDatabase.py
+++ ReadDatabase.py
@@ -10,7 +10,7 @@ conn = sqlite3.connect('db/books.db')
 c = conn.cursor()
 
 excel_path = 'Database/Database.xlsx'
-
+vids_path = 'WordsVids/Vids/'
 # Folha1/Folha2: Excel em Português / Sheet1/Sheet2: Excel em Inglês
 Dataframe1 = pd.read_excel(excel_path, 'Folha1')
 Dataframe2 = pd.read_excel(excel_path, 'Folha2')
@@ -27,12 +27,22 @@ Palavras_EN = pd.DataFrame(Dataframe2, columns=['Palavras_EN'])
 Palavras_PT = pd.DataFrame(Dataframe2, columns=['Palavras_PT'])
 
 # https://stackoverflow.com/questions/19472922/reading-external-sql-script-in-python
+
 with open('db/createdb.sql', 'r') as sql_file:
-    c.execute("INSERT INTO BOOK(TITLE, IMG) VALUES('Titulo_Livro', 'Imagem') ;")
-    c.execute("SELECT * FROM BOOK;")
-    c.execute("INSERT INTO PAGES(NUM, IMG, BOOK) VALUES('Página', 'Imagem', 'Titulo_Livro') ;")
-    c.execute("INSERT INTO PAGE_CONTENTS(PT, EN, PAGEID) VALUES('Texto_EN', 'Texto_PT','Página') ;")
-    c.execute("INSERT INTO WORDS(WORD_EN,WORD_PT, FIG_PATH) VALUES('Palavras_EN', 'Palavras_PT', 'Imagem') ;")
-    conn.commit()
+    for index, row in Dataframe1.iterrows():
+        c.execute("INSERT INTO BOOK(TITLE, IMG) values(?,?)",
+                  (row.Livro, row.Imagem))
+        c.execute("INSERT INTO PAGES(NUM, IMG, BOOK) values(?,?,?)",
+                  (row.Página, row.Imagem, row.Livro))
+        c.execute("INSERT INTO PAGE_CONTENTS(PT, EN, PAGEID) values(?,?,?)",
+                  (row.Texto_EN, row.Texto_PT, row.Página))
+        conn.commit()
+
+    for index, row in Dataframe2.iterrows():
+        video_name = row.Palavras_EN.replace('"', '')
+        string_video = vids_path + video_name + '.mp4'
+        c.execute("INSERT INTO WORDS(WORD_EN,WORD_PT,FIG_PATH) values(?,?,?)",
+                  (row.Palavras_EN, row.Palavras_PT, string_video))
+        conn.commit()
 
 conn.close()
